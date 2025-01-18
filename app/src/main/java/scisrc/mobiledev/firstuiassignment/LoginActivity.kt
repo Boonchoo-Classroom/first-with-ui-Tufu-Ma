e30bccb8934d8689fab8c75cddbe7e3b16c6693e
Tufu-Ma <surachai.ma@ku.th>
2025-01-19 01:00:02 +0700

    finish

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
@@ -1,7 +1,6 @@
 package scisrc.mobiledev.firstuiassignment
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +8,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var backToMainBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +18,6 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
